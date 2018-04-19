98b28755190108e56676141ce6945b5ae4a4ca41
zhenya.zhang <zhhzhfya@163.com>
2018-04-19 13:23:59 +0800

    add

---
+++ readme.txt
@@ -0,0 +1,5 @@
+
+gin_demo2>go build src/main/main.go
+
+#编译到bin下
+gin_demo2\src\main>go install
---
+++ src/myfunc/a.go
@@ -0,0 +1,9 @@
+package myfunc
+
+import "fmt"
+
+func Afunc(str string) {
+	fmt.Println("a.go is package mufunc.")
+	fmt.Println(str)
+}
+
